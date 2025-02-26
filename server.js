71f59c3b006bca3a588a190e2508888c3ede5e26
AmrMa3owad <161595269+AmrMa3owad@users.noreply.github.com>
2025-02-26 16:56:45 +0200

    ..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+/*const PORT = process.env.PORT || 4000;*/
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,7 +12,7 @@ app.get("/api/message", (req, res) => {
     res.json({ message: "Hello from Node.js backend!" });
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+//// Start the server
+//app.listen(PORT, () => {
+//    console.log(`Server is running on http://localhost:${PORT}`);
+//});
